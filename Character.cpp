443e3f5259a816db54c82b128a0030f31538ee13
CharlesP <charles.partous@gmail.com>
2024-02-08 13:49:37 -0500

    -finished dwefault Character constructor -add Print_Character_Sheet

--- Character.cpp
+++ Character.cpp
@@ -1,31 +1,88 @@
 #include "Character.h"
-#include <iostream>
-#include <random>
 
-Character::Character(int t_level, CharacterClass t_class){
-	int skill_points = (2 + ability_modifiers[intelligence]) * 4;
-	//-add exception handeling for level && class
-	character_class = t_class;
-	level = t_level;
+Character::Character::Character(){
+	//create random number generator
+	std::random_device rd;
+
+	//Baseline class for assignment 1
+	character_class = FIGHTER;
+	//Generate level
+	level = rd() % 20 + 1;
 	//Generate ability scores
 	for (int i{ 0 }; i < 6; i++) {
-		//ability scores randomly generated between 1 & 18
-		ability_scores[i] = rand() % 18 + 1;
+		//roll 3d6 for each ability score
+		for (int j{ 0 }; j < 3; j++) {
+			ability_scores[i] += rd() % 6 + 1;
+		}
 	}
-	//Calculate ability modifiers
+	//Generate ability modifiers
 	for (int i{ 0 }; i < 6; i++) {
-		ability_modifiers[i] = std::floor((ability_scores[i] - 10) / 2);
+		ability_modifiers[i] = std::floor(((float)(ability_scores[i] - 10) / 2));
 	}
-	//Calculate hit points
+	//Generate hit points
+	for (int i{ 0 }; i < level; i++) {
+		hit_points += (rd() % 10 + 1)+ability_modifiers[CONSTITUTION];
+	}
+	//Generate base_armour_class
+	natural_armour_class = (10 + ability_modifiers[DEXTERITY]);
+	//Generate proficiency_bonus
+	proficiency_bonus = std::lround((double)(level / 5.0)) + 2;
+	//Generate attack_bonus (assume unarmed so use STRENGTH mod)
+	attack_bonus = ability_modifiers[STRENGTH] + proficiency_bonus;
+	//Generate damage_bonus(assume_unarmed at start so 0)
+	damage_bonus = 0;
+}
+
+void Character::Character::Print_Character_Sheet()
+{
+	std::cout << std::right << std::setw(25) << "Name: " << name<<std::endl;
+	std::cout << std::right << std::setw(25) << "Class: " << get_class_name() << std::endl;
+	std::cout << std::right << std::setw(25) << "Level: " << level << std::endl;
+	std::cout << std::right << std::setw(25) << "Proficiency Bonus : " << proficiency_bonus << std::endl;
+	std::cout << std::right << std::setw(25) << "Attack Bonus (Unarmed) : " << attack_bonus << std::endl;
+	std::cout << std::endl << std::right << std::setw(20) << "Ability Scores: " << std::endl << std::endl;
+	std::cout << std::right << std::setw(20) << "Strength: " << std::left << std::setw(8) <<ability_scores[STRENGTH] << std::left << std::setw(1) << "Modifier: " << ability_modifiers[STRENGTH] << std::endl;
+	std::cout << std::right << std::setw(20) << "Dexterity: " << std::left << std::setw(8) << ability_scores[DEXTERITY] << std::left << std::setw(1) << "Modifier: " << ability_modifiers[DEXTERITY] << std::endl;
+	std::cout << std::right << std::setw(20) << "Constitution: " << std::left << std::setw(8) << ability_scores[CONSTITUTION] << std::left << std::setw(1) << "Modifier: " << ability_modifiers[CONSTITUTION] << std::endl;
+	std::cout << std::right << std::setw(20) << "Intelligence: " << std::left << std::setw(8) << ability_scores[INTELLIGENCE] << std::left << std::setw(1) << "Modifier: " << ability_modifiers[INTELLIGENCE] << std::endl;
+	std::cout << std::right << std::setw(20) << "Wisdom: " << std::left << std::setw(8) << ability_scores[WISDOM] << std::left << std::setw(1) << "Modifier: " << ability_modifiers[WISDOM] << std::endl;
+	std::cout << std::right << std::setw(20) << "Charisma: " << std::left << std::setw(8) << ability_scores[CHARISMA] << std::left << std::setw(1) << "Modifier: " << ability_modifiers[CHARISMA] << std::endl;
+
+
+
+
+
+
+}
+
+std::string Character::Character::get_class_name()
+{
+	switch (this->character_class)
+	{
+	case BARBARIAN:
+		return "Barbarian";
+	case BARD:
+		return "Bard";
+	case CLERIC:
+		return "Cleric";
+	case DRUID:
+		return "Druid";
+	case FIGHTER:
+		return "Fighter";
+	case MONK:
+		return "Monk";
+	case PALADIN:
+		return "Paladin";
+	case RANGER:
+		return "Ranger";
+	case ROGUE:
+		return "Rogue";
+	case SORCERER:
+		return "Sorcerer";
+	case WIZARD:
+		return "Wizard";
+	default:
+		return "Unknown Class";
+	}
 	
-	//Character creation based on class 
-	switch (character_class){
-		case fighter:
-			//Fighter class case 
-			std::cout << "is fighter" << std::endl;
-			break;
-		default:
-			std::cout << "INVALID CLASS" << std::endl;
-			return;
-	}
 }
--- Character.h
+++ Character.h
@@ -1,52 +1,72 @@
 #pragma once
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <chrono>
+//#include <time.h>
 
-enum CharacterClass{
-	barbarian,
-	bard,
-	cleric,
-	druid,
-	fighter,
-	monk,
-	paladin,
-	ranger,
-	rogue,
-	sorcerer,
-	wizard
-};
+namespace Character {
 
-enum Abilities {
+	enum Character_Class {
+		BARBARIAN,
+		BARD,
+		CLERIC,
+		DRUID,
+		FIGHTER,
+		MONK,
+		PALADIN,
+		RANGER,
+		ROGUE,
+		SORCERER,
+		WIZARD
+	};
 
-	strength = 0,
-	dexterity = 1,
-	constitution = 2,
-	intelligence = 3,
-	wisdom = 4,
-	charisma = 5,
-};
+	enum Abilities {
 
-struct EquipmentSlots {
-	// Equiped items must be changed to the item data type
-	int armor{};
-	int shield{};
-	int weapon{};
-	int boots{};
-	int ring{};
-	int helmet{};
-};
+		STRENGTH = 0,
+		DEXTERITY = 1,
+		CONSTITUTION = 2,
+		INTELLIGENCE = 3,
+		WISDOM = 4,
+		CHARISMA = 5,
+	};
 
-class Character {
-public:
-	Character(int level, CharacterClass c_class);
+	class Character {
+	public:
+		Character();
+		void Print_Character_Sheet();
+		const int Hit_Points() { return hit_points; };
+		const int Attack_Bonus() { return attack_bonus; };
+		const int Modifier(Abilities t_ability) { return ability_modifiers[t_ability]; };
+		const int Proficiency_Bonus() { return proficiency_bonus; };
 
-private:
-	CharacterClass character_class{};
-	int level;
-	//Can use 'Ability' enum to index to the right ability score
-	int ability_scores[6]{};
-	int ability_modifiers[6]{};
-	EquipmentSlots equipment_slots{};
-	int hit_points{};
-	int armour_class{}; 
-	int attack_bonus{};
-	int damage_bonus{};
-};
+
+	private:
+		//Default name
+		std::string name{ "Cirian" };
+		struct EquipmentSlots {
+			// Equiped items must be changed to the item data type
+			int armor{};
+			int shield{};
+			int weapon{};
+			int boots{};
+			int ring{};
+			int helmet{};
+		};
+
+		Character_Class character_class{};
+		int level;
+		//Can use 'Ability' enum to index to the right ability score
+		int ability_scores[6]{};
+		int ability_modifiers[6]{};
+		EquipmentSlots equipment_slots{};
+		int hit_points{};
+		int natural_armour_class{};
+		int armour_class{};
+		int attack_bonus{};
+		int damage_bonus{};
+		int proficiency_bonus{};
+
+		std::string get_class_name();
+	};
+}
