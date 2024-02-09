b12e15f86ee654f800895c6d4810710ac8a8eef3
CharlesP <charles.partous@gmail.com>
2024-02-09 13:36:43 -0500

    - changed some stuff

--- Character.cpp
+++ Character.cpp
@@ -4,10 +4,15 @@ Character::Character::Character(){
 	//create random number generator
 	std::random_device rd;
 
-	//Baseline class for assignment 1
-	character_class = FIGHTER;
+	//Baseline class for assignment 1 is FIGHTER
 	//Generate level
-	level = rd() % 20 + 1;
+	level.at(FIGHTER) = rd() % 20 + 1;
+	//Set character_classes
+	for (int i = 0 ; i<level.size();i++) {
+		if (level[i] > 0) {
+			character_class.set(i);
+		}
+	}
 	//Generate ability scores
 	for (int i{ 0 }; i < 6; i++) {
 		//roll 3d6 for each ability score
@@ -20,13 +25,13 @@ Character::Character::Character(){
 		ability_modifiers[i] = std::floor(((float)(ability_scores[i] - 10) / 2));
 	}
 	//Generate hit points
-	for (int i{ 0 }; i < level; i++) {
+	for (int i{ 0 }; i < Sum_Levels(); i++) {
 		hit_points += (rd() % 10 + 1)+ability_modifiers[CONSTITUTION];
 	}
 	//Generate base_armour_class
-	natural_armour_class = (10 + ability_modifiers[DEXTERITY]);
+	armour_class = (10 + ability_modifiers[DEXTERITY]);
 	//Generate proficiency_bonus
-	proficiency_bonus = std::lround((double)(level / 5.0)) + 2;
+	proficiency_bonus = std::ceil((double)(Sum_Levels() / 4.0)) + 1;
 	//Generate attack_bonus (assume unarmed so use STRENGTH mod)
 	attack_bonus = ability_modifiers[STRENGTH] + proficiency_bonus;
 	//Generate damage_bonus(assume_unarmed at start so 0)
@@ -36,8 +41,14 @@ Character::Character::Character(){
 void Character::Character::Print_Character_Sheet()
 {
 	std::cout << std::right << std::setw(25) << "Name: " << name<<std::endl;
-	std::cout << std::right << std::setw(25) << "Class: " << get_class_name() << std::endl;
-	std::cout << std::right << std::setw(25) << "Level: " << level << std::endl;
+	std::cout << std::right << std::setw(25) << "Class: ";
+	for (int i = 0; i < 11; i++) {
+		if (character_class.test(i)) {
+			std::cout << Get_Class_Name(i) << ", ";
+		}
+	}
+	std::cout << std::endl;
+	std::cout << std::right << std::setw(25) << "Level: " << Sum_Levels() << std::endl;
 	std::cout << std::right << std::setw(25) << "Proficiency Bonus : " << proficiency_bonus << std::endl;
 	std::cout << std::right << std::setw(25) << "Attack Bonus (Unarmed) : " << attack_bonus << std::endl;
 	std::cout << std::endl << std::right << std::setw(20) << "Ability Scores: " << std::endl << std::endl;
@@ -48,16 +59,63 @@ void Character::Character::Print_Character_Sheet()
 	std::cout << std::right << std::setw(20) << "Wisdom: " << std::left << std::setw(8) << ability_scores[WISDOM] << std::left << std::setw(1) << "Modifier: " << ability_modifiers[WISDOM] << std::endl;
 	std::cout << std::right << std::setw(20) << "Charisma: " << std::left << std::setw(8) << ability_scores[CHARISMA] << std::left << std::setw(1) << "Modifier: " << ability_modifiers[CHARISMA] << std::endl;
 
-
-
-
-
-
 }
 
-std::string Character::Character::get_class_name()
+const int Character::Character::Sum_Levels()
 {
-	switch (this->character_class)
+	int sum{ 0 };
+	for (int i{ 0 };i<level.size();i++) {
+		sum += level[i];
+	}
+	return sum;
+}
+
+bool Character::Character::Levelup(Character_Class t_class)
+{
+	switch (t_class)
+	{
+	case BARBARIAN:
+		return true;
+		break;
+	case BARD:
+		return true;
+		break;
+	case CLERIC:
+		return true;
+		break;
+	case DRUID:
+		return true;
+		break;
+	case FIGHTER:
+		return true;
+		break;
+	case MONK:
+		return true;
+		break;
+	case PALADIN:
+		return true;
+		break;
+	case RANGER:
+		return true;
+		break;
+	case ROGUE:
+		return true;
+		break;
+	case SORCERER:
+		return true;
+		break;
+	case WIZARD:
+		return true;
+		break;
+	default:
+		return false;
+		break;
+	}
+}
+
+std::string Character::Character::Get_Class_Name(Character_Class t_class)
+{
+	switch (t_class)
 	{
 	case BARBARIAN:
 		return "Barbarian";
@@ -86,3 +144,36 @@ std::string Character::Character::get_class_name()
 	}
 	
 }
+
+std::string Character::Character::Get_Class_Name(int t_class)
+{
+	switch (t_class)
+	{
+	case 0:
+		return "Barbarian";
+	case 1:
+		return "Bard";
+	case 2:
+		return "Cleric";
+	case 3:
+		return "Druid";
+	case 4:
+		return "Fighter";
+	case 5:
+		return "Monk";
+	case 6:
+		return "Paladin";
+	case 7:
+		return "Ranger";
+	case 8:
+		return "Rogue";
+	case 9:
+		return "Sorcerer";
+	case 10:
+		return "Wizard";
+	default:
+		return "Unknown Class";
+	}
+
+}
+
--- Character.h
+++ Character.h
@@ -3,7 +3,12 @@
 #include <iomanip>
 #include <random>
 #include <chrono>
-//#include <time.h>
+#include <vector>
+#include <bitset>
+
+class Item {
+
+};
 
 namespace Character {
 
@@ -39,34 +44,44 @@ namespace Character {
 		const int Attack_Bonus() { return attack_bonus; };
 		const int Modifier(Abilities t_ability) { return ability_modifiers[t_ability]; };
 		const int Proficiency_Bonus() { return proficiency_bonus; };
-
+		const int Sum_Levels();
+		const std::bitset<11> Get_Class() { return character_class; };
+		bool Levelup(Character_Class t_class);
+		bool Equipe_Item(Item* t_item);
 
 	private:
 		//Default name
 		std::string name{ "Cirian" };
 		struct EquipmentSlots {
 			// Equiped items must be changed to the item data type
-			int armor{};
-			int shield{};
-			int weapon{};
-			int boots{};
-			int ring{};
-			int helmet{};
+			Item* armor{};
+			Item* shield{};
+			Item* weapon{};
+			Item* boots{};
+			Item* ring{};
+			Item* helmet{};
 		};
 
-		Character_Class character_class{};
-		int level;
-		//Can use 'Ability' enum to index to the right ability score
+		std::bitset<11> character_class;
+		std::vector<int> level = std::vector<int> (11,0);
+		//Use 'Ability' enum to index to the right ability score
 		int ability_scores[6]{};
 		int ability_modifiers[6]{};
-		EquipmentSlots equipment_slots{};
 		int hit_points{};
-		int natural_armour_class{};
 		int armour_class{};
 		int attack_bonus{};
 		int damage_bonus{};
 		int proficiency_bonus{};
 
-		std::string get_class_name();
+		EquipmentSlots equipment_slots{};
+		std::string Get_Class_Name(Character_Class t_class);
+		std::string Get_Class_Name(int t_class);
+
+		/*
+		int calculate_armour_class();
+		int calculate_proficiency();
+		int calculate_attack_bonus();
+		*/
+
 	};
 }
