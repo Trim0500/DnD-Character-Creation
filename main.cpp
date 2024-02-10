96933be723ab7ffd21a91b8e595133afaf45313b
Tristan Lafleur <70395022+Trim0500@users.noreply.github.com>
2024-02-10 17:29:23 -0500

    Feat[COMP345-A1-TestItem]: Test cases for Item

--- main.cpp
+++ main.cpp
@@ -2,10 +2,29 @@
 //
 
 #include <iostream>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Get the top level suite from the registry
+	CppUnit::Test* suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+
+	// Adds the test to the list of test to run
+	CppUnit::TextUi::TestRunner runner;
+	runner.addTest(suite);
+
+	// Change the default outputter to a compiler error format outputter
+	runner.setOutputter(new CppUnit::CompilerOutputter(&runner.result(),
+		std::cerr));
+	// Run the tests.
+	bool wasSucessful = runner.run();
+
+	getchar();
+
+	// Return error code 1 if the one of test failed.
+	return wasSucessful ? 0 : 1;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
--- testitem.cpp
+++ testitem.cpp
@@ -9,42 +9,42 @@
 
 using namespace std;
 
-//void TestItem::setUp(void)
-//{
-//	noArgsItemObject = new Item();
-//
-//	customItemObject = new Item("testItem", 3, 4, 1);
-//}
-//
-//void TestItem::tearDown(void)
-//{
-//	delete noArgsItemObject;
-//
-//	delete customItemObject;
-//}
-//
-//void TestItem::TestNoArgsItemConstructor(void) {
-//	CPPUNIT_ASSERT(noArgsItemObject->GetItemName() == "");
-//	CPPUNIT_ASSERT(noArgsItemObject->GetEnchantmentBonus() >= 1 && noArgsItemObject->GetEnchantmentBonus() <= 5);
-//	CPPUNIT_ASSERT(noArgsItemObject->GetItemType() >= 1 && noArgsItemObject->GetItemType() <= 7);
-//	CPPUNIT_ASSERT(noArgsItemObject->GetEnchantmentType() >= 1 && noArgsItemObject->GetEnchantmentType() <= 9);
-//}
-//
-//void TestItem::TestItemConstructor(void) {
-//	CPPUNIT_ASSERT(customItemObject->GetItemName() == "testItem");
-//	CPPUNIT_ASSERT(customItemObject->GetEnchantmentBonus() == 3);
-//	CPPUNIT_ASSERT(customItemObject->GetItemType() == Ring);
-//	CPPUNIT_ASSERT(customItemObject->GetEnchantmentType() == Strength);
-//}
-//
-//void TestItem::TestGetEnchantmentBonus(void) {
-//	CPPUNIT_ASSERT(noArgsItemObject->GetEnchantmentBonus() >= 1 && noArgsItemObject->GetEnchantmentBonus() <= 5);
-//
-//	CPPUNIT_ASSERT(customItemObject->GetEnchantmentBonus() == 3);
-//}
-//
-//void TestItem::TestGetEnchantmentType(void) {
-//	CPPUNIT_ASSERT(noArgsItemObject->GetEnchantmentType() >= 1 && noArgsItemObject->GetEnchantmentType() <= 9);
-//
-//	CPPUNIT_ASSERT(customItemObject->GetEnchantmentType() == Strength);
-//}
+void TestItem::setUp(void)
+{
+	noArgsItemObject = new Item();
+
+	customItemObject = new Item("testItem", 3, 4, 1);
+}
+
+void TestItem::tearDown(void)
+{
+	delete noArgsItemObject;
+
+	delete customItemObject;
+}
+
+void TestItem::TestNoArgsItemConstructor(void) {
+	CPPUNIT_ASSERT(noArgsItemObject->GetItemName() == "");
+	CPPUNIT_ASSERT(noArgsItemObject->GetEnchantmentBonus() >= 1 && noArgsItemObject->GetEnchantmentBonus() <= 5);
+	CPPUNIT_ASSERT(noArgsItemObject->GetItemType() >= 1 && noArgsItemObject->GetItemType() <= 7);
+	CPPUNIT_ASSERT(noArgsItemObject->GetEnchantmentType() >= 1 && noArgsItemObject->GetEnchantmentType() <= 9);
+}
+
+void TestItem::TestItemConstructor(void) {
+	CPPUNIT_ASSERT(customItemObject->GetItemName() == "testItem");
+	CPPUNIT_ASSERT(customItemObject->GetEnchantmentBonus() == 3);
+	CPPUNIT_ASSERT(customItemObject->GetItemType() == Ring);
+	CPPUNIT_ASSERT(customItemObject->GetEnchantmentType() == Strength);
+}
+
+void TestItem::TestGetEnchantmentBonus(void) {
+	CPPUNIT_ASSERT(noArgsItemObject->GetEnchantmentBonus() >= 1 && noArgsItemObject->GetEnchantmentBonus() <= 5);
+
+	CPPUNIT_ASSERT(customItemObject->GetEnchantmentBonus() == 3);
+}
+
+void TestItem::TestGetEnchantmentType(void) {
+	CPPUNIT_ASSERT(noArgsItemObject->GetEnchantmentType() >= 1 && noArgsItemObject->GetEnchantmentType() <= 9);
+
+	CPPUNIT_ASSERT(customItemObject->GetEnchantmentType() == Strength);
+}
