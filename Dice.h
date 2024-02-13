27762d6f14c9d5bc06c3bb20070e6e7d8b0ec827
shadowlerone <andrae.lewis@gmail.com>
2024-02-13 14:56:30 -0500

    dice update

--- Dice.h
+++ Dice.h
@@ -12,10 +12,40 @@ using namespace std;
 static regex DICE("([0-9]+)d(4|6|8|10|12|20|100)((?:\\+|-)[0-9]*)*");
 class Dice {
 public:
+	Dice(string query);
 	Dice();
-	int roll(string query);
+	static int roll(string query);
+	int roll();
+private:
+	int leading_multiplier;
+	int die_sides;
+	int addition;
 };
+Dice::Dice(string query){
+	smatch sm; // rename variables
+	if (!regex_search(query, sm, DICE)) {
+		string e = "Query '" + query +  "' is invalid\n";
+		throw invalid_argument(e);
+	}
+	if (sm[3].matched) {
+		const string s = sm[3];
+		if (s != ""){
+			this->addition = stoi(s);
+		}
+	}
+	string leading_multiplier_string = sm[1];
+	string die_sides_string = sm[2];
+	this->leading_multiplier = stoi(leading_multiplier_string);
+	this->die_sides = stoi(die_sides_string);
 
+}
+int Dice::roll(){
+	int rolls = 0;
+	for (int i = 0; i < leading_multiplier; i++){
+		rolls += (rand() % die_sides + 1); 
+	}
+	return rolls + addition;
+}
 Dice::Dice() {
 	srand(time(nullptr));
 }
