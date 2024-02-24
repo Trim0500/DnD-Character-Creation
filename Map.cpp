9820d836ab030b9d9b09533ac23fdaed0142ccd2
Polomich <99100218+Polomich@users.noreply.github.com>
2024-02-24 17:27:41 -0500

    add path finding algo

--- Map.cpp
+++ Map.cpp
@@ -1,28 +1,20 @@
 #include "Map.h"
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <list>
+#include <queue>
 
 //basic constructor
 Map::Map::Map(int r, int c) {
 	rows = r;
 	cols = c;
-
-	//grid.resize(rows); // resize how many rows
+	end_cell[0] = r - 1;
+	end_cell[1] = c - 1;
 
 	for (int i = 0; i < rows; i++) {
 		grid.push_back(vector<Cell_Type>(cols, Cell_Type::empty));
 	}
-
-	/*push back every row to have proper amount of cols
-	for (int i = 0; i < rows; i++) {
-		grid[i].resize(cols, empty);
-	}*/
-
-	/*for (int i = 0; i < Rows(); i++) {
-		for (int j = 0; j < Cols(); j++) {
-			grid[i][j] = empty;
-		}
-	}*/
 }
 
 Map::Map* Map::Map::Create() {
@@ -36,6 +28,10 @@ Map::Map* Map::Map::Create() {
 	Map* map = new Map(rows, cols);
 
 	map->Customize();
+	map->Print();
+	cout << "check path" << endl;
+	bool path = map->IsTherePath();
+	cout << path;
 
 	return map;
 }
@@ -52,15 +48,15 @@ void Map::Map::Customize() {
 		cin >> row;
 		cout << "Enter the cell column you want to change: ";
 		cin >> col;
-		cout << "Enter the cell type you want to change it to (e, w, i): ";
+		cout << "Enter the cell type you want to change it to (e, w, s): ";
 		cin >> celltype;
 
 		type = ConvertToCellType(celltype);
 
-		this->grid[row][col] = type;
+		grid[row][col] = type;
 
 		cout << "Your map looks like this: " << endl;
-		this->Print();
+		Print();
 
 		stop = KeepCustomizing();
 
@@ -94,8 +90,8 @@ Map::Cell_Type Map::ConvertToCellType(char letter) {
 		return Cell_Type::empty;
 	case 'w':
 		return Cell_Type::wall;
-	case 'i':
-		return Cell_Type::item;
+	case 's':
+		return Cell_Type::special;
 	default:
 		cout << "Not a cell type. Defaults to 'empty'.";
 		return Cell_Type::empty;
@@ -103,10 +99,65 @@ Map::Cell_Type Map::ConvertToCellType(char letter) {
 
 }
 
+bool Map::Map::IsTherePath() {
+	// start = 0,0
+	// end = lower right cell, ie: cols.length-1, rows.lenght-1
+	//int curr[2] = { 0,0 }; //current checking cell
+	//end_cell = {r-1, c-1}
+	bool found = false;
+
+	//initial list of visited cells (none to begin with)
+	vector<vector<bool>> visited(rows, std::vector<bool>(cols, false));
+
+	//BFS queue
+	queue<pair<int, int>> q;
+
+	//start cell is visited (0,0)
+	q.push({ 0,0 });
+	visited[0][0] = true;
+	cout << "initial values" << endl;
+
+	while (!q.empty()) {
+		auto curr = q.front(); // current checking cell
+		q.pop();
+
+		cout << "checking: " << curr.first << ", " << curr.second << endl;
+
+		//check if at the end cell
+		if (curr.first == end_cell[0] && curr.second == end_cell[1]) {
+			cout << "almost done" << endl;
+			return true;
+		}
+
+		//keep searching
+		for (const auto& dir : dirs) {
+			cout << "keep searching" << endl;
+			int nextRow = curr.first + dir.first;
+			int nextCol = curr.second + dir.second;
+
+			cout << "nextrow: " << nextRow << " nextcol: " << nextCol << endl;
+
+			//check if next cell is not a wall or outside the map or been visited
+			if (ValidCell(nextRow, nextCol, visited) {
+				q.push({ nextRow, nextCol });
+				visited[nextRow][nextCol] = true;
+				visited[nextRow][nextCol] = true;
+			}
+		}
+	}
+
+	return false;
+
+}
+
+bool Map::Map::ValidCell(int nextRow, int nextCol, vector<vector<bool>> visited) {
+	return (nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols && grid[nextRow][nextCol] == Cell_Type::empty && !visited[nextRow][nextCol]);
+}
+
 //print the grid
 void Map::Map::Print() {
-	for (int i = 0; i < Rows(); i++) {
-		for (int j = 0; j < Cols(); j++) {
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
 			cout << static_cast<char>(grid[i][j]) << " ";
 		}
 		cout << endl;
