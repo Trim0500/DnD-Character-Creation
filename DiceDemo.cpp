8d570f540c38b375a0bad0899e224f432f109ca2
shadowlerone <andrae.lewis@gmail.com>
2024-02-24 20:25:12 -0500

    removed while loop
    while loop doesn't allow for build

--- DiceDemo.cpp
+++ DiceDemo.cpp
@@ -6,17 +6,19 @@ void DiceDemo(){
 	int r;
 	Dice d;
 	string query;
-	while (true){ 
+	
+	//while (true) {
 		cout << "Input dice to roll: (! to return to main menu)" << endl;
 		cin >> query;
-		if (query.rfind("!", 0)) {break;}
+		///if (query.rfind("!", 0) == 0) { break; }
+		cout << query;
 		try {
 			d = Dice(query);
 			r = d.roll(query);
 			cout << "You rolled " << r << endl;
-		} catch (invalid_argument exc) {
+		}
+		catch (invalid_argument exc) {
 			cout << "Invalid query" << endl;
 		}
-	}
-	
+	//}
 };
