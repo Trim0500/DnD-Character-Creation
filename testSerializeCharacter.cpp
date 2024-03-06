02ca3046ada25e11ce90224c1a1f949cc092ac58
CharlesP <charles.partous@gmail.com>
2024-03-06 14:30:26 -0500

    add unit tests for character serialization

---
+++ testSerializeCharacter.cpp
@@ -0,0 +1,43 @@
+#include "testSerializeCharacter.h"
+
+void TestSerializeCharacter::setUp(void)
+{
+	characterObject = new Character::Character();
+}
+
+void TestSerializeCharacter::tearDown(void)
+{
+	delete characterObject;
+	characterObject = nullptr;
+}
+
+void TestSerializeCharacter::TestSaveCharacter(void)
+{
+	CPPUNIT_ASSERT(serializecharacter::SaveCharacter(characterObject, ""));
+}
+
+void TestSerializeCharacter::TestLoadCharacter(void)
+{
+	serializecharacter::SaveCharacter(characterObject, "");
+	filename = serializecharacter::FindCharacterFile(characterObject->ID(), "");
+	serializecharacter::CharacterRecord record = serializecharacter::LoadCharacter(filename);
+	CPPUNIT_ASSERT(record.id == characterObject->ID());
+	CPPUNIT_ASSERT(record.name == characterObject->Name());
+	CPPUNIT_ASSERT(record.max_hit_points == characterObject->Max_Hit_Points());
+	CPPUNIT_ASSERT(record.hit_points == characterObject->Hit_Points());
+	for (int i{ 0 };i < record.level.size();i++) {
+		CPPUNIT_ASSERT(record.level.at(i) == characterObject->Levels((Character::Character_Class)i));
+	}
+	for (int i{ 0 }; i < record.ability_scores.size(); i++) {
+		CPPUNIT_ASSERT(record.ability_scores.at(i) == characterObject->Ability_Score_Natural((Character::Abilities_Stats)i));
+	}
+
+}
+
+void TestSerializeCharacter::TestFindCharacterFile(void)
+{
+	serializecharacter::SaveCharacter(characterObject, "");
+	std::string testName = "Character_" + std::to_string(characterObject->ID()) + ".csv";
+	filename = serializecharacter::FindCharacterFile(characterObject->ID(),"");
+	CPPUNIT_ASSERT(testName.compare(filename));
+}
