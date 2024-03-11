11b04c9b3685b21ab38a138a2be766b9d245d11c
shadowlerone <andrae.lewis@gmail.com>
2024-03-10 20:45:35 -0400

    added observer and observable base classes

---
+++ ProjectSourceCode/Observable.h
@@ -0,0 +1,17 @@
+#include <list>
+#include "Observer.h"
+
+class Observable {
+	public: 
+		void attach (Observer* o){_observers->push_back(o);}
+		void detach (Observer* o){_observers->remove(o);}
+		void notify (){
+			// std::list<Observer*>::iterator it;
+			for (auto const&i : *_observers){
+				i->update((void*)this);
+			}
+		}
+
+	private:
+		std::list<Observer*> *_observers;
+};
