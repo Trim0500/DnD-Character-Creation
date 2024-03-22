3bc445ae78901b6ee3bf45f6f535352c143f2f03
Tristan Lafleur <70395022+Trim0500@users.noreply.github.com>
2024-03-21 20:02:54 -0400

    Feat[COMP345-A3-66]: Algos for aggressor and friendly

--- ProjectSourceCode/friendlystrategy.cpp
+++ ProjectSourceCode/friendlystrategy.cpp
@@ -1,12 +1,55 @@
 #include "friendlystrategy.h"
+#include "Character.h"
+#include "Wall.h"
+#include "EmptyCell.h"
+
+using namespace item;
+using namespace itemcontainer;
 
 namespace {
 
 }
 
 namespace friendlystrategy {
-    std::vector<CellActionInfo> FriendlyStrategy::UseMovementStrategy(std::vector<std::vector<Interactable::Interactable*>>& _currentGrid, const int&, const int&) const {
+    std::vector<CellActionInfo> FriendlyStrategy::UseMovementStrategy(std::vector<std::vector<Interactable::Interactable*>>& _currentMap, const int& _row, const int& _col) const {
         std::vector<CellActionInfo> actions;
+
+        for (int i = 0; i < 4; ++i)
+        {
+            int rowToCheck = _row;
+            int colToCheck = _col;
+            
+            if (i = 0) {
+                colToCheck += 1;
+            }
+            else if (i = 1) {
+                rowToCheck += 1;
+            }
+            else if (i = 2) {
+                colToCheck -= 1;
+            }
+            else {
+                rowToCheck -= 1;
+            }
+
+            Interactable::Interactable* valueAtCell = _currentMap[rowToCheck][colToCheck];
+            CellActionInfo actionInfo;
+            
+            if (dynamic_cast<Wall*>(valueAtCell)) {
+                actionInfo.row = rowToCheck;
+                actionInfo.col = colToCheck;
+                actionInfo.cellColor = "N/A";
+                actionInfo.actionName = "None";
+            }
+            else {
+                actionInfo.row = rowToCheck;
+                actionInfo.col = colToCheck;
+                actionInfo.cellColor = "Blue";
+                actionInfo.actionName = "Move";
+            }
+
+            actions.push_back(actionInfo);
+        }
 
         return actions;
     }
