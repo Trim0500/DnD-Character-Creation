dc0b73c2b6de6a108a61cd4f7721382bb80d58b8
Tristan Lafleur <70395022+Trim0500@users.noreply.github.com>
2024-03-28 16:45:33 -0400

    Feat[COMP345-39]: Attack game logger to observables

--- ProjectSourceCode/gamelogger.h
+++ ProjectSourceCode/gamelogger.h
@@ -3,14 +3,16 @@
 #include <string>
 
 #include "Observer.h"
+#include "Observable.h"
 
 using namespace observer;
+using namespace observable;
 
 namespace gamelogger
 {
     class GameLogger : public Observer {
         public:
-            GameLogger(){};
+            GameLogger(Observable* _observable) { _observable->Attach(this); };
 
             virtual ~GameLogger(){};
 
