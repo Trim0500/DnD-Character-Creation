a52ba4c906d92201834bf321bdbeda7c6d9e5b1a
Tristan Lafleur <70395022+Trim0500@users.noreply.github.com>
2024-03-29 11:31:37 -0400

    Feat[COMP345-39]: Dice logging

--- ProjectSourceCode/gamelogger.cpp
+++ ProjectSourceCode/gamelogger.cpp
@@ -3,8 +3,7 @@
 #include <string>
 #include <filesystem>
 #include <sstream>
-#include <chrono>
-#include <ctime>
+#include <time.h>
 
 #include "gamelogger.h"
 
@@ -12,11 +11,12 @@
 
 namespace {
     std::string CreateLogMessage(const std::string& _observableMessage) {
-        auto currentTime = std::chrono::system_clock::now();
-        std::time_t formmattedTime = std::chrono::system_clock::to_time_t(currentTime);
+        time_t now = time(NULL);
+        char dateTimeString[26] = {};
+        ctime_s(dateTimeString, 26, &now);
 
         std::ostringstream logMessage;
-        logMessage <<  "[" << std::ctime(&formmattedTime) << "] " << _observableMessage;
+        logMessage <<  "[" << dateTimeString << "] " << _observableMessage;
 
         return logMessage.str();
     }
