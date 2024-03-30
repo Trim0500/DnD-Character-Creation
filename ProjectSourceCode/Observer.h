97f82ff59d3ed9392cc65f8e9cc64ee92957d458
Tristan Lafleur <70395022+Trim0500@users.noreply.github.com>
2024-03-30 14:50:04 -0400

    Chore[COMP345-39]: Documentation for observer

--- ProjectSourceCode/Observer.h
+++ ProjectSourceCode/Observer.h
@@ -1,14 +1,41 @@
+/*!
+* \file Observer.h
+* \brief File that establishes the interface for concrete implementations
+*
+* @author Tristan Lafleur (40245238)
+*/
+
 #include <string>
 
 #pragma once
 
+/*!
+* \namespace observer
+* \brief Namespace to partition the Observer functionality
+*/
 namespace observer {
+	/*!
+    * \class Observer
+    * \brief Class to abstract the observer functionality
+    */
 	class Observer {
 		public:
+			/*!
+            * \fn ~Observer
+            * \brief Destructor for Campaign
+            */
 			virtual ~Observer() {};
 
+			/*!
+            * \fn update
+            * \brief Abstract function to provide concrete implementations a way to override using static string
+            */
 			virtual void update(std::string) = 0;
 			
+			/*!
+            * \fn update
+            * \brief Abstract function to provide concrete implementations a way to override using void pointers
+            */
 			virtual void update(void*) const = 0;
 	};
 }
