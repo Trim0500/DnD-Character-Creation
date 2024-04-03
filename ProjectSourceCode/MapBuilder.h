cb482a0583eac5c6c1c1f16b30889779455e629f
polomich <michpolonsky@gmail.com>
2024-04-03 00:19:39 -0400

    delete unused code

--- ProjectSourceCode/MapBuilder.h
+++ ProjectSourceCode/MapBuilder.h
@@ -10,18 +10,9 @@ namespace MapBuilder {
 	class MapBuilder {
 
 	public:
-		// get built Map from Builder
-		//Map::Map* getMap() { return m_map; }
-		//build generic void Map
-		//void createnewMap() { m_map = new Map::Map(); }
-
 		//save map object to a file
 		static bool SaveMap(Map::Map* map, std::string& filename);
 		//load map from a text file
 		static Map::Map LoadMap(std::string& filename);
-
-	//protected:
-	//	//Product built by Builder
-	//	Map::Map* m_map;
 	};
 }
